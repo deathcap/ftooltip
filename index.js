8487dcac68bc0d054c79adc978b48ac62bccfee5
deathcap <deathcap@gmx.co.uk>
2014-03-01 12:00:15 -0800

    Try precreating tooltip nodes, but it is ridiculously slows because of the clientHeight calculation
    Ref GH-1

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function Tooltip(node, opts) {
     'pointer-events: none;',
     'color: white;',
     'z-index: 20;',
+    'visibility: hidden;',
     opts.extraStyle || '',
     ].join('\n');
 
@@ -29,6 +30,8 @@ function Tooltip(node, opts) {
 }
 
 Tooltip.prototype.enable = function() {
+  this.create();
+
   this.node.addEventListener('mouseover', this.onMouseover = this.show.bind(this));
   this.node.addEventListener('mouseout', this.onMouseout = this.hide.bind(this));
 };
@@ -36,6 +39,11 @@ Tooltip.prototype.enable = function() {
 Tooltip.prototype.disable = function() {
   this.node.removeEventListener('mouseover', this.onMouseover);
   this.node.removeEventListener('mouseout', this.onMouseout);
+
+  if (this.div) {
+    this.div.parentNode.removeChild(this.div);
+    delete this.div;
+  }
 };
 
 Tooltip.prototype.create = function() {
@@ -55,6 +63,7 @@ Tooltip.prototype.create = function() {
 }
 
 Tooltip.prototype.show = function(ev) {
+  this.div.style.visibility = '';
   this.move(ev.x, ev.y);
   this.node.addEventListener('mousemove', this.onMousemove = this.track.bind(this));
 };
@@ -64,19 +73,12 @@ Tooltip.prototype.track = function(ev) {
 }
 
 Tooltip.prototype.move = function(x, y) {
-  if (!this.div) {
-    this.create();
-  }
-
   this.div.setAttribute('style', this.style);
   this.div.style.left = x + 'px';
   this.div.style.top = (y - this.divHeight) + 'px';
 };
 
 Tooltip.prototype.hide = function() {
+  this.div.style.visibility = 'hidden';
   this.node.removeEventListener('mousemove', this.onMousemove);
-  if (this.div) {
-    this.div.parentNode.removeChild(this.div);
-    delete this.div;
-  }
 };
