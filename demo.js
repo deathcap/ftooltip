ad381ef1e8bc0bd7c484811226a718e05620c1d3
deathcap <deathcap@gmx.co.uk>
2014-03-01 12:57:42 -0800

    Update demo

--- demo.js
+++ demo.js
@@ -8,9 +8,10 @@ node.style.width = '100px';
 node.style.height = '100px';
 document.body.appendChild(node);
 
-//ftooltip(node, 'hello world');
-var t = document.createTextNode('hello world');
-ftooltip(node, t);
+ftooltip(node, 'hello world');
+//var t = document.createTextNode('hello world');
+//ftooltip(node, t);
+//t.textContent = 'hello world 2'; // demonstrate changing after the fact
 //ftooltip(node, {info: 'hello world'});
 //ftooltip(node, ['hello world', document.createElement('br'), 'second line']);
 //ftooltip(node, {info:['hello world', document.createElement('br'), 'second line']});
